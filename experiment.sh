abdbca8935777462a03ff7298aecc31ffd53dfa6
Jeff Zhuo <jeff87@oprime.cs.unc.edu>
2024-03-03 21:21:16 -0500

    fix script typo

--- experiment.sh
+++ experiment.sh
@@ -3,7 +3,7 @@
 videos=("clip1" "clip2" "clip3" "clip4" "clip5")
 conda activate MixSort2
 for vid in "${videos[@]}"; do
-    python3 tools/demo_track.py \
+    python3 tools/demo_track_per_player.py \
     -expn yolox_x_sports_mix \
     -f exps/example/mot/yolox_x_sportsmot.py \
     -c pretrained/yolox_x_sports_mix.pth.tar \
